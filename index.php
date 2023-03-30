d08d507fd071332331ccc632622366be07999ab9
v4lt3rsmn <v4lt3rsmn@gmail.com>
2023-03-30 00:20:16 -0300

    first php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $hello = "Hello World!";
+    $name = "Valter";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php echo($hello) ?>
+    </h1>
+    <p>
+        <?php echo($name) ?>
+    </p>
+</body>
+</html>
